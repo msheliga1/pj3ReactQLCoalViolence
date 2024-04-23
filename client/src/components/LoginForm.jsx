499fd43c6a515f6e0e20e87bc98459dcf4bb4da7
Mike Sheliga <msheliga1@gmail.com>
2024-04-23 13:52:46 -0400

    Got Login to work. Awesome. Act26 Login.jsx had to combine formHandler with LoginForm.jsx. Kept validator, showAlert useStates. Super awesome this works after signUp problems.

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,22 +1,35 @@
 // see SignupForm.js for comments
 import { useState } from 'react';
+// next line NOT in Login26.jsx
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+// Next line commented out 
+// import { loginUser } from '../utils/API';
+// next 3 lines added from Login26.jsx 
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
-const LoginForm = () => {
-  const [userFormData, setUserFormData] = useState({ email: '', password: '' });
-  const [validated] = useState(false);
-  const [showAlert, setShowAlert] = useState(false);
+// const LoginForm = () => {
+// const [userFormData, setUserFormData] = useState({ email: '', password: '' });
+const LoginForm = (props) => {
+  const [validated] = useState(false);  // kept from starter code, else render err.
+  const [showAlert, setShowAlert] = useState(false); // kept from starter code, else render err
+  // no username when logging in. 
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error, data }] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    setUserFormData({ ...userFormData, [name]: value });
+    // setUserFormData({ ...userFormData, [name]: value });
+    setFormState({...formState, [name]: value,});
   };
 
+  // submit form 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
+    console.log("LoginForm handleFormSubit starting ...  with formState ", formState); 
 
     // check if form has everything (as per react-bootstrap docs)
     const form = event.currentTarget;
@@ -25,27 +38,40 @@ const LoginForm = () => {
       event.stopPropagation();
     }
 
-    try {
-      const response = await loginUser(userFormData);
+    try {  // from Act 26
+      console.log("LoginForm handleFormSubit calling login-LOGIN_USER with formState ", formState); 
+      // This line seems to call useMutation(LOGIN_USER)  
+      const { data } = await login({variables: { ...formState },});
+      console.log("LoginForm handleFormSubit returned form login-LOGIN_USER with data ", data); 
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.log("LoginForm handleFormSubit err after login-LOGIN_USER "); 
+      console.error(e);
+      setShowAlert(true);
+    }  // end try-catch
 
+    /* // old from REST starter code 
+    try { 
+      console.log("LoginForm handleFormSubit calling LOGIN_USER with userFormData ", userFormData); 
+      const response = await loginUser(userFormData);
+      console.log("LoginForm handleFormSubit got LOGIN_USER response: ", response); 
       if (!response.ok) {
         throw new Error('something went wrong!');
       }
-
       const { token, user } = await response.json();
       console.log(user);
       Auth.login(token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
-    }
+    } // end try-catch */ 
 
-    setUserFormData({
-      username: '',
+    setFormState({
+      // username: '',
       email: '',
       password: '',
-    });
-  };
+    }); // end setFormState 
+  }; // end handleFormSubmit
 
   return (
     <>
@@ -60,7 +86,7 @@ const LoginForm = () => {
             placeholder='Your email'
             name='email'
             onChange={handleInputChange}
-            value={userFormData.email}
+            value={formState.email}
             required
           />
           <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
@@ -73,20 +99,20 @@ const LoginForm = () => {
             placeholder='Your password'
             name='password'
             onChange={handleInputChange}
-            value={userFormData.password}
+            value={formState.password}
             required
           />
           <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
         </Form.Group>
         <Button
-          disabled={!(userFormData.email && userFormData.password)}
+          disabled={!(formState.email && formState.password)}
           type='submit'
           variant='success'>
           Submit
         </Button>
       </Form>
     </>
-  );
-};
+  );  // end return html-react 
+};  // end login-form
 
 export default LoginForm;
