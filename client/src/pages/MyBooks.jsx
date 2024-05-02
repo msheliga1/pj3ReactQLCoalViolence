bf51996275906c25e1bc80baf99357dee4e540aa
Mike Sheliga <msheliga1@gmail.com>
2024-05-02 17:47:24 -0400

    Added MyBooks.jsx and NewFight.jsx. Login still works (FOR NOW ARGHHH.).

---
+++ client/src/pages/MyBooks.jsx
@@ -0,0 +1,160 @@
+// MJS 5.2.24 - client/src/pages/MyBooks.jsx from hw21 savedBooks - uri starterCode. 
+// Goal: Display Fights user has created. 
+import { useState, useEffect } from 'react';
+import { Container, Card, Button, Row, Col } from 'react-bootstrap';
+
+import { getMe, deleteBook } from '../utils/API';
+import Auth from '../utils/auth';
+import { removeBookId } from '../utils/localStorage';
+import { useQuery, useMutation } from '@apollo/client';
+import { GET_ME, GET_ME_ALL } from '../utils/queries'; 
+import { REMOVE_BOOK } from '../utils/mutations'; 
+
+// Remove the useEffect() Hook that sets the state for UserData.
+// Instead, use the useQuery() Hook to run the GET_ME query on load and save it to a variable named userData.
+const MyBooks = () => {
+  console.log("Starting MyBooks ..."); 
+  // it seems that userData is what is displayed in the html 
+  const [userData, setUserData] = useState({});  // here is the user localStorage variable
+
+  const [removeBook, { error }] = useMutation(REMOVE_BOOK); // must be outside handleXXX method
+  const { loading, data } = useQuery(GET_ME_ALL, { variables: { }, });  
+
+  console.log("MyBooks GET_ME_ALL returned data ", data); 
+  if (!data) { 
+    console.log("MyBooks userQuery(GET_ME_ALL) no data: ", data); 
+    return false; 
+  }
+  if (!data.me) { 
+    console.log("MyBooks userQuery(GET_ME_ALL) no data.me: ", data.me); 
+    return false; 
+  }
+  if (!data.me.username) { 
+    console.log("MyBooks userQuery(GET_ME_ALL) no data.me.username: ", data.me.username); 
+    return false; 
+  }
+  const userMe = data.me; 
+  // userMe = { ...userMe, savedBooks:[]} ;  // for testing when GET_ME didnt have savedBooks
+  console.log("Saved books. user from GET_ME_ALL data.me: ", userMe); 
+  console.log("Saved books. user from GET_ME_ALL data.me.username: ", userMe.username); 
+  console.log("Saved books. user from GET_ME_ALL data.me.savedBooks.length: ", userMe.savedBooks.length); 
+
+  // This occassionally gives "More hooks than previous render (even after rebuild and server restart)
+  // But the seems work sometimes. MJS 4.25.24
+  // Problem seems to occur after logging out and logging back in.  
+  const justMe = useQuery(GET_ME, { variables: {  }, });  // justMe has loading and data fields
+  console.log("MyBooks GET_ME returned ", justMe); 
+  const data3 = justMe.data; 
+  console.log("MyBooks GET_ME returned data ", data3); 
+  const us3 = data3.me; 
+  console.log("MyBooks GET_ME returned data.me ", us3); 
+
+  // This seems to work. MJS 4.25.24 
+  /* const getMeAll = useQuery(GET_ME_ALL, { variables: { }, });  
+  console.log("MyBooks getMeAll = useQuery(...) is ", getMeAll); 
+  const loading2 = getMeAll.loading;
+  const data2 = getMeAll.data;   
+  console.log("MyBooks getMeAll data  ", data2);  */ 
+
+  // use this to determine if `useEffect()` hook needs to run again
+  const userDataLength = Object.keys(userData).length;
+
+  useEffect(() => {
+    const getUserData = async () => {
+      console.log("Staring MyBooks - getUserData..."); 
+      try {
+        const token = Auth.loggedIn() ? Auth.getToken() : null;
+        if (!token) {
+          console.log("Saved books. Could not get token. returning."); 
+          return false;
+        }
+        console.log("MyBooks found token: ", token.substring(0,4), "..."); 
+        // As suspected, cant call useQuery(GET_ME) hook here. 
+        // Error: hooks can only be called inside body of function component.
+
+        /* const response = await getMe(token);
+        if (!response.ok) { throw new Error('something went wrong!');  }
+        const user = await response.json(); */ 
+        setUserData(userMe);
+      } catch (err) {
+        console.log("MyBooks getUserData error: ", err); 
+        console.error(err);
+      }
+    };  // end getUserData
+    getUserData();
+    // next line means run method when userDataLength changes, as well as page load
+  }, [userDataLength]);  // end useEffect
+
+  // create function that accepts the book's mongo _id value as param and deletes the book from the database
+  const handleDeleteBook = async (bookId) => {
+    console.log("MyBooks.jsx handleDeleteBook starting ... "); 
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+    if (!token) {
+      console.log("SavedBoooks handleDeleteBook. Could not get token. "); 
+      return false;
+    }
+    try {
+      // const response = await deleteBook(bookId, token);
+      // if (!response.ok) { throw new Error('something went wrong!'); }
+      // const updatedUser = await response.json();
+      const username = userMe.username; 
+      const vars = { username, bookId }; 
+      console.log("MyBooks handleDeleteBook deleting book using ", vars); // correct
+      const { data } = await removeBook({variables: vars,});
+      console.log("MyBooks handleDeleteBook saveBook data ", data); 
+      const dataRemoved = data.removeBook;  // this is actually the updated User
+      const booksLeft = dataRemoved.savedBooks.length; 
+      console.log("MyBooks handleDeleteBook username: ", dataRemoved.username, " savedBookCount ", booksLeft);
+      setUserData(dataRemoved);  // originally updatedUser
+      console.log("MyBooks handleDeleteBook reset UserData for ", dataRemoved.username);
+      // upon success, remove book's id from localStorage
+      removeBookId(bookId); 
+      console.log("MyBooks handleDeleteBook book removed from local storage ", bookId);
+    } catch (err) {
+      console.error(err);
+    }
+  }; // end handleDeleteBook 
+
+  // if data isn't here yet, say so
+  if (!userDataLength) {
+    return <h2>LOADING...</h2>;
+  }
+
+  return (
+    <>
+      <div fluid className="text-light bg-dark p-5">
+        <Container>
+          <h1>Viewing Your Incidents</h1>
+        </Container>
+      </div>
+      <Container>
+        <h2 className='pt-5'>
+          {userData.savedBooks.length
+            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
+            : 'You have no saved books!'}
+        </h2>
+        <Row>
+          {userData.savedBooks.map((book) => {
+            return (
+              <Col md="4">
+                <Card key={book.bookId} border='dark'>
+                  {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
+                  <Card.Body>
+                    <Card.Title>{book.title}</Card.Title>
+                    <p className='small'>Authors: {book.authors}</p>
+                    <Card.Text>{book.description}</Card.Text>
+                    <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                      Update this Incident!
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>  
+            );
+          })}
+        </Row>
+      </Container>
+    </>
+  ); // end return html-react
+}; // end const savedBooks => ... 
+
+export default MyBooks;
---
+++ client/src/pages/NewFight.jsx
@@ -0,0 +1,221 @@
+// MJS 2.23.24 - client/src/pages/SearchBooks.jsx from uri starter code. 
+// This code not only searches for books, it allows one to save each found book. 
+// Goal: Convert REST to GraphQL 
+import { useState, useEffect } from 'react';
+import { Container, Col, Form, Button, Card, Row } from 'react-bootstrap';
+import Auth from '../utils/auth';
+
+// import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';  // likely need to search Google
+import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+// next 3 lines from commentForm26 
+import { Link } from 'react-router-dom';
+import { useQuery, useMutation } from '@apollo/client';
+// import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
+import { GET_ME } from '../utils/queries';
+
+const SearchBooks = ( ) => { 
+  // create state for holding returned google api data
+  const [searchedBooks, setSearchedBooks] = useState([]);
+  // create state for holding our search field data
+  const [searchInput, setSearchInput] = useState('');
+  // create state to hold saved bookId values
+  const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+  // Added next line for GraphQL. Basically convert SAVE_BOOK graphQL into saveBook method. 
+  const [saveBook, { error }] = useMutation(SAVE_BOOK); // must be outside handleForm promise
+  // Sample from SingleThough26 
+  // const { loading, data } = useQuery(QUERY_SINGLE_THOUGHT, { variables: { thoughtId: thoughtId }, });
+  // Added next line for GraphQL. Basically find out who is logged in. // must be outside handleForm promise
+  // God only knows why this works with "data" but not "dataMe". 
+  const { loading, data } = useQuery(GET_ME, { variables: {  }, });  
+  console.log("Searchbook GET_ME returned data ", data); 
+
+  let good = false; 
+  if (data) {
+    if (data.me) {
+      if (data.me.username) {
+        console.log("SearchBooks GET_ME result contains data.me.username"); 
+        good = true;
+      }
+    }
+  }
+
+  let username = "MJS"; 
+  if (good) {
+    username = data.me.username;
+  } else {
+    console.log("Search Books query GET_USER returned no data: ", data); 
+    console.log("Setting username to MJS"); 
+  } 
+
+  console.log("Searchbook GET_ME returned username ", username);  // works with data
+
+  // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
+  // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
+  useEffect(() => {
+    return () => saveBookIds(savedBookIds);
+  });  // end useEffect
+
+  // create method to search for books and set state on form submit
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    if (!searchInput) {
+      return false;
+    }
+    try {
+      const response = await searchGoogleBooks(searchInput);
+      if (!response.ok) {
+        throw new Error('something went wrong searching for a book. ' + searchInput);
+      }
+      const { items } = await response.json();
+      const bookData = items.map((book) => ({
+        bookId: book.id,
+        authors: book.volumeInfo.authors || ['No author to display'],
+        title: book.volumeInfo.title,
+        description: book.volumeInfo.description,
+        image: book.volumeInfo.imageLinks?.thumbnail || '',
+      }));
+      setSearchedBooks(bookData);
+      setSearchInput('');
+    } catch (err) {
+      console.error(err);
+    } // end try-catch 
+  };  // end handleFormSubmit 
+
+  // create function to handle saving a book to our database
+  const handleSaveBook = async (bookId) => {
+    // find the book in `searchedBooks` state by the matching id
+    const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
+    console.log("SearchBooks.jsx handleSaveBook saving for userID ", username); 
+    console.log("SearchBooks.jsx handleSaveBook title ", bookToSave.title, " gBookId ", bookToSave.bookId); 
+    // get token
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+    if (!token) {
+      return false;
+    }
+    try {
+      console.log("SearchBooks.jsx handleSaveBook got token ... saving book using token ", token); 
+      // GraphQL method from CommentForm26 
+      // const { data } = await addComment({
+      //  variables: { thoughtId, commentText, commentAuthor: Auth.getProfile().data.username, }, });
+      // setCommentText('');
+      const vars = { username: username, ...bookToSave}; 
+      console.log("SearchBooks.jsx handleSaveBook saving book using ", vars); 
+      //  const { data } = await login({variables: { ...formState },}); // Analagous line from LoginForm.jsx 
+      const { data } = await saveBook({variables: vars,});
+      console.log("Saved book. Returned data: ", data); 
+      // if book successfully saves to user's account, save book id (only) to state
+      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+    } catch (err) {
+      console.error(err);
+    }
+  }; // end handleSaveBook 
+
+  // create function to handle saving a book to our database
+  const handleCreateBook = async (bookId) => {
+    // find the book in `searchedBooks` state by the matching id
+    const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
+    console.log("SearchBooks.jsx handleSaveBook saving for userID ", username); 
+    console.log("SearchBooks.jsx handleSaveBook title ", bookToSave.title, " gBookId ", bookToSave.bookId); 
+    // get token
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+    if (!token) {
+      return false;
+    }
+    try {
+      console.log("SearchBooks.jsx handleCreateBook got token ... creating book using token ", token); 
+      // GraphQL method from CommentForm26 
+      // const { data } = await addComment({
+      //  variables: { thoughtId, commentText, commentAuthor: Auth.getProfile().data.username, }, });
+      // setCommentText('');
+      const vars = { username: username, ...bookToSave}; 
+      console.log("SearchBooks.jsx handleSaveBook saving book using ", vars); 
+      //  const { data } = await login({variables: { ...formState },}); // Analagous line from LoginForm.jsx 
+      const { data } = await saveBook({variables: vars,});
+      console.log("Saved book. Returned data: ", data); 
+      // if book successfully saves to user's account, save book id (only) to state
+      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+    } catch (err) {
+      console.error(err);
+    }
+  }; // end handleCreateBook 
+
+  return (
+    <>
+      <div className="text-light bg-dark p-5">
+        <Container>
+          <h1>Search for Books!</h1>
+          <Form onSubmit={handleFormSubmit}>
+            <Row>
+              <Col xs={12} md={8}>
+                <Form.Control
+                  name='searchInput'
+                  value={searchInput}
+                  onChange={(e) => setSearchInput(e.target.value)}
+                  type='text'
+                  size='lg'
+                  placeholder='Search for a book'
+                />
+              </Col>
+              <Col xs={12} md={4}>
+                <Button type='submit' variant='success' size='lg'>
+                  Submit Search
+                </Button>
+              </Col>
+            </Row>
+          </Form>
+        </Container>
+      </div>
+
+      <Container>
+        <h2 className='pt-5'>
+          {searchedBooks.length
+            ? `Viewing ${searchedBooks.length} results:`
+            : 'Search for a book to begin'}
+        </h2>
+        <Row>
+          {searchedBooks.map((book) => {
+            return (
+              <Col md="4" key={book.bookId}>
+                <Card border='dark'>
+                  {book.image ? (
+                    <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' />
+                  ) : null}
+                  <Card.Body>
+                    <Card.Title>{book.title}</Card.Title>
+                    <p className='small'>Authors: {book.authors}</p>
+                    <Card.Text>{book.description}</Card.Text>
+                    {Auth.loggedIn() && (
+                      <Button
+                        disabled={savedBookIds?.some((savedBookId) => savedBookId === book.bookId)}
+                        className='btn-block btn-info'
+                        onClick={() => handleSaveBook(book.bookId)}>
+                        {savedBookIds?.some((savedBookId) => savedBookId === book.bookId)
+                          ? 'This book has already been saved!'
+                          : 'Save this Book!'}
+                      </Button>
+                    )}                    
+                    {Auth.loggedIn() && (
+                      <Button
+                        disabled={savedBookIds?.some((savedBookId) => savedBookId === book.bookId)}
+                        className='btn-block btn-info'
+                        onClick={() => handleCreateBook(book.bookId)}>
+                        {savedBookIds?.some((savedBookId) => savedBookId === book.bookId)
+                          ? 'This book has already been saved!'
+                          : 'Save this Book!'}
+                      </Button>
+                    )}
+
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default SearchBooks;
