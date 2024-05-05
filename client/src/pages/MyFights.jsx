93e42a8c4e344da4a5c5373a9d7c0eebc95a1a2f
Mike Sheliga <msheliga1@gmail.com>
2024-05-04 22:41:00 -0400

    Changed book to fight in client/src/pages/MyFights.jsx.  Tested added new book and viewed in MyFights.

--- client/src/pages/MyFights.jsx
+++ client/src/pages/MyFights.jsx
@@ -1,4 +1,4 @@
-// MJS 5.2.24 - client/src/pages/MyBooks.jsx from hw21 savedBooks - uri starterCode. 
+// MJS 5.2.24 - client/src/pages/MyFights.jsx from hw21 savedBooks - uri starterCode. 
 // Goal: Display Fights user has created. 
 import { useState, useEffect } from 'react';
 import { Container, Card, Button, Row, Col } from 'react-bootstrap';
@@ -13,24 +13,24 @@ import { REMOVE_BOOK } from '../utils/mutations';
 // Remove the useEffect() Hook that sets the state for UserData.
 // Instead, use the useQuery() Hook to run the GET_ME query on load and save it to a variable named userData.
 const MyFights = () => {
-  console.log("Starting MyBooks ..."); 
+  console.log("Starting MyFights ..."); 
   // it seems that userData is what is displayed in the html 
   const [userData, setUserData] = useState({});  // here is the user localStorage variable
 
   const [removeBook, { error }] = useMutation(REMOVE_BOOK); // must be outside handleXXX method
   const { loading, data } = useQuery(GET_ME_ALL, { variables: { }, });  
 
-  console.log("MyBooks GET_ME_ALL returned data ", data); 
+  console.log("MyFights GET_ME_ALL returned data ", data); 
   if (!data) { 
-    console.log("MyBooks userQuery(GET_ME_ALL) no data: ", data); 
+    console.log("MyFights userQuery(GET_ME_ALL) no data: ", data); 
     return false; 
   }
   if (!data.me) { 
-    console.log("MyBooks userQuery(GET_ME_ALL) no data.me: ", data.me); 
+    console.log("MyFights userQuery(GET_ME_ALL) no data.me: ", data.me); 
     return false; 
   }
   if (!data.me.username) { 
-    console.log("MyBooks userQuery(GET_ME_ALL) no data.me.username: ", data.me.username); 
+    console.log("MyFights userQuery(GET_ME_ALL) no data.me.username: ", data.me.username); 
     return false; 
   }
   const userMe = data.me; 
@@ -42,32 +42,32 @@ const MyFights = () => {
   // But the seems work sometimes. MJS 4.25.24
   // Problem seems to occur after logging out and logging back in.  
   const justMe = useQuery(GET_ME, { variables: {  }, });  // justMe has loading and data fields
-  console.log("MyBooks GET_ME returned ", justMe); 
+  console.log("MyFights GET_ME returned ", justMe); 
   const data3 = justMe.data; 
-  console.log("MyBooks GET_ME returned data ", data3); 
+  console.log("MyFights GET_ME returned data ", data3); 
   const us3 = data3.me; 
-  console.log("MyBooks GET_ME returned data.me ", us3); 
+  console.log("MyFights GET_ME returned data.me ", us3); 
 
   // This seems to work. MJS 4.25.24 
   /* const getMeAll = useQuery(GET_ME_ALL, { variables: { }, });  
-  console.log("MyBooks getMeAll = useQuery(...) is ", getMeAll); 
+  console.log("MyFights getMeAll = useQuery(...) is ", getMeAll); 
   const loading2 = getMeAll.loading;
   const data2 = getMeAll.data;   
-  console.log("MyBooks getMeAll data  ", data2);  */ 
+  console.log("MyFights getMeAll data  ", data2);  */ 
 
   // use this to determine if `useEffect()` hook needs to run again
   const userDataLength = Object.keys(userData).length;
 
   useEffect(() => {
     const getUserData = async () => {
-      console.log("Staring MyBooks - getUserData..."); 
+      console.log("Staring MyFights - getUserData..."); 
       try {
         const token = Auth.loggedIn() ? Auth.getToken() : null;
         if (!token) {
           console.log("Saved books. Could not get token. returning."); 
           return false;
         }
-        console.log("MyBooks found token: ", token.substring(0,4), "..."); 
+        console.log("MyFights found token: ", token.substring(0,4), "..."); 
         // As suspected, cant call useQuery(GET_ME) hook here. 
         // Error: hooks can only be called inside body of function component.
 
@@ -76,7 +76,7 @@ const MyFights = () => {
         const user = await response.json(); */ 
         setUserData(userMe);
       } catch (err) {
-        console.log("MyBooks getUserData error: ", err); 
+        console.log("MyFights getUserData error: ", err); 
         console.error(err);
       }
     };  // end getUserData
@@ -85,11 +85,11 @@ const MyFights = () => {
   }, [userDataLength]);  // end useEffect
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
-  const handleDeleteBook = async (bookId) => {
-    console.log("MyBooks.jsx handleDeleteBook starting ... "); 
+  const handleUpdateFight = async (bookId) => {
+    console.log("MyFights.jsx handleUpdateFight starting ... "); 
     const token = Auth.loggedIn() ? Auth.getToken() : null;
     if (!token) {
-      console.log("SavedBoooks handleDeleteBook. Could not get token. "); 
+      console.log("MyFights handleUpdateFight. Could not get token. "); 
       return false;
     }
     try {
@@ -98,21 +98,21 @@ const MyFights = () => {
       // const updatedUser = await response.json();
       const username = userMe.username; 
       const vars = { username, bookId }; 
-      console.log("MyBooks handleDeleteBook deleting book using ", vars); // correct
+      console.log("MyFights handleUpdateFight deleting book using ", vars); // correct
       const { data } = await removeBook({variables: vars,});
-      console.log("MyBooks handleDeleteBook saveBook data ", data); 
+      console.log("MyFights handleUpdateFight saveBook data ", data); 
       const dataRemoved = data.removeBook;  // this is actually the updated User
       const booksLeft = dataRemoved.myFights.length; 
-      console.log("MyBooks handleDeleteBook username: ", dataRemoved.username, " savedBookCount ", booksLeft);
+      console.log("MyFights handleUpdateFight username: ", dataRemoved.username, " savedBookCount ", booksLeft);
       setUserData(dataRemoved);  // originally updatedUser
-      console.log("MyBooks handleDeleteBook reset UserData for ", dataRemoved.username);
+      console.log("MyFights handleUpdateFight reset UserData for ", dataRemoved.username);
       // upon success, remove book's id from localStorage
       removeBookId(bookId); 
-      console.log("MyBooks handleDeleteBook book removed from local storage ", bookId);
+      console.log("MyFights handleUpdateFight book removed from local storage ", bookId);
     } catch (err) {
       console.error(err);
     }
-  }; // end handleDeleteBook 
+  }; // end handleUpdateFight 
 
   // if data isn't here yet, say so
   if (!userDataLength) {
@@ -123,14 +123,14 @@ const MyFights = () => {
     <>
       <div fluid className="text-light bg-dark p-5">
         <Container>
-          <h1>Viewing Incidents You Created</h1>
+          <h1>Viewing Historical Coal Camp Incidents You Created</h1>
         </Container>
       </div>
       <Container>
         <h2 className='pt-5'>
           {userData.myFights.length
-            ? `Viewing ${userData.myFights.length} saved ${userData.myFights.length === 1 ? 'book' : 'books'}:`
-            : 'You have not created any books!'}
+            ? `Viewing ${userData.myFights.length} saved ${userData.myFights.length === 1 ? 'incident' : 'incidents'}:`
+            : 'You have not created any historical coal camp violence incidents!'}
         </h2>
         <Row>
           {userData.myFights.map((book) => {
@@ -142,7 +142,7 @@ const MyFights = () => {
                     <Card.Title>{book.title}</Card.Title>
                     <p className='small'>Authors: {book.authors}</p>
                     <Card.Text>{book.description}</Card.Text>
-                    <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                    <Button className='btn-block btn-danger' onClick={() => handleUpdateFight(book.bookId)}>
                       Update this Incident!
                     </Button>
                   </Card.Body>
