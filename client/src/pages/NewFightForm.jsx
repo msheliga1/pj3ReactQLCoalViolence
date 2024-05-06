e24a8726061e4ad2521819942a2510e061aaa813
Mike Sheliga <msheliga1@gmail.com>
2024-05-05 22:14:27 -0400

    Restarted NewFightForm using SignUpForm

--- client/src/pages/NewFightForm.jsx
+++ client/src/pages/NewFightForm.jsx
@@ -1,196 +1,119 @@
-// MJS 5.2.24 - client/src/pages/NewFightFrom.jsx from MJS hw21 and uri starter code. 
-// Goal: Get create working.  Then create a form to accept data. 
-import { useState, useEffect } from 'react';
-import { Container, Col, Form, Button, Card, Row } from 'react-bootstrap';
+import { useState } from 'react';
+import { Form, Button, Alert } from 'react-bootstrap';
+
+// First change compared to Act26 (at one time was looking at WRONG file)
+// import { createUser } from '../utils/API';
+import { ADD_USER2 } from '../utils/mutations';  // correct name 4.23.24
+import { useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
 
-// import { saveBook, searchGoogleBooks } from '../utils/API';
-import { searchGoogleBooks } from '../utils/API';  // likely need to search Google
-import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
-// next 3 lines from commentForm26 
-import { Link } from 'react-router-dom';
-import { useQuery, useMutation } from '@apollo/client';
-// import { useMutation } from '@apollo/client';
-import { SAVE_BOOK } from '../utils/mutations';
-import { CREATE_BOOK } from '../utils/mutations';
-import { GET_ME } from '../utils/queries';
+const NewFightForm = () => {
+  // set initial form state
+  const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+  // set state for form validation
+  const [validated] = useState(false);
+  // set state for alert
+  const [showAlert, setShowAlert] = useState(false);
+  // "Convert" ADD_USER2 mutation to addUser method
+  const [addUser, { error, data }] = useMutation(ADD_USER2);
 
-const NewFightForm = ( ) => { 
-  // create state for holding returned google api data
-  const [searchedBooks, setSearchedBooks] = useState([]);
-  // create state for holding our search field data
-  const [searchInput, setSearchInput] = useState('');
-  // create state to hold saved bookId values
-  const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
-  // const [createBook, { error }] = useMutation(CREATE_BOOK); // must be outside handleForm promise
-  // const [addThought, { error }] = useMutation(ADD_THOUGHT, 
-  //   {refetchQueries: [QUERY_THOUGHTS, 'getThoughts', QUERY_ME, 'me']});
-  const [createBook, { error }] = useMutation(CREATE_BOOK, 
-        {refetchQueries: [GET_ME, 'me']});
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserFormData({ ...userFormData, [name]: value });
+  }; 
 
-  // Sample from SingleThough26 
-  // const { loading, data } = useQuery(QUERY_SINGLE_THOUGHT, { variables: { thoughtId: thoughtId }, });
-  // Added next line for GraphQL. Basically find out who is logged in. // must be outside handleForm promise
-  // God only knows why this works with "data" but not "dataMe". 
-  const { loading, data } = useQuery(GET_ME, { variables: {  }, });  
-  console.log("NewFight GET_ME returned data ", data); 
-
-  let good = false; 
-  if (!!data && data.me && data.me.username) {
-        console.log("SearchBooks GET_ME result contains data.me.username"); 
-        good = true;
-      }
-  let username = "MJS"; 
-  if (good) {
-    username = data.me.username;
-  } else {
-    console.log("NewFightForm query GET_USER returned no data: ", data); 
-    console.log("Setting username to MJS"); 
-  } 
-  console.log("NewFightForm GET_ME returned username ", username);  // works with data
-
-  // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
-  // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
-  useEffect(() => {
-    return () => saveBookIds(savedBookIds);
-  });  // end useEffect
-
-  // create method to create new record (and insert ptr to it in User.myFights)
   const handleFormSubmit = async (event) => {
+    console.log("handleFormSubmit (SignUpForm.jsx) starting ... "); 
     event.preventDefault();
-    console.log("NewFightForm.jsx handleFormSubmit saving for userID ", username); 
-    // get token
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
-    if (!token) {  return false;    }
+    // check if form has everything (as per react-bootstrap docs)
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
     try {
-        console.log("NewFight.jsx handleCreateBook got token ... creating book using token ", token); 
-        // Create the book to save by copying form data to vars 
-        const title = "Book One"; 
-        const bookId = "7449292";
-        const description = "Book One Desc"; 
-        const authors = [ "Author1" ]; 
-        const image = null; 
-        const link = null;
-        const comments = []; 
-        const bookToSave = { title, bookId, authors, description, image, link, comments }; 
-        console.log("NewFightForm.jsx handleFormSubmit title ", bookToSave.title, " gBookId ", bookToSave.bookId); 
-        const vars = { username: username, ...bookToSave}; 
-        console.log("NewFight.jsx handleCreateBook creating book using ", vars); 
-        //  const { data } = await login({variables: { ...formState },}); // Analagous line from LoginForm.jsx 
-        //  thoughtAuthor: Auth.getProfile().data.username,
-        const { data } = await createBook({variables: vars,});
-        console.log("Created book. Returned data: ", data); 
-        // if book successfully saves to user's account, save book id (only) to state
-        setSavedBookIds([...savedBookIds, bookToSave.bookId]);
-        // setThoughtText('');
-      } catch (err) {
-        console.error(err);
-      }
-  };  // end handleFormSubmit 
+      // These lines generally changed REST->GQL 
+      // const response = await createUser(userFormData);
+      console.log("handleFormSubmit trying to ADD_USER2 with userFromData ", userFormData);
+      // Next line a poor convert of old method.  Can't treeat ADD_USER2(pararm) as a method. 
+      // const response = await ADD_USER2(userFormData); // No Good 
+      const { data } = await addUser({variables: { ...userFormData },});
+      console.log("handleFormSubmit returned from addUser done: ", data); 
 
-  // create function to handle creating a book to our database. Also create ptr in User
-  const handleCreateBook = async (bookId) => {
-    // find the book in `searchedBooks` state by the matching id
-    const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
-    console.log("NewFight.jsx handleCreateBook saving for userID ", username); 
-    console.log("NewFight.jsx handleCreateBook title ", bookToSave.title, " gBookId ", bookToSave.bookId); 
-    // get token
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
-    if (!token) {
-      return false;
-    }
-    try {
-      console.log("NewFight.jsx handleCreateBook got token ... creating book using token ", token); 
-      // GraphQL method from CommentForm26 
-      // const { data } = await addComment({
-      //  variables: { thoughtId, commentText, commentAuthor: Auth.getProfile().data.username, }, });
-      // setCommentText('');
-      const vars = { username: username, ...bookToSave}; 
-      console.log("NewFight.jsx handleCreateBook creating book using ", vars); 
-      //  const { data } = await login({variables: { ...formState },}); // Analagous line from LoginForm.jsx 
-      const { data } = await createBook({variables: vars,});
-      console.log("Created book. Returned data: ", data); 
-      // if book successfully saves to user's account, save book id (only) to state
-      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+      Auth.login(data.addUser.token);
+      // Auth.login(token);
+      console.log("handleFormSubmit returned from login done: ");
     } catch (err) {
+      console.log("handleFromSubmit catch an err ... setting ShowAlert to true, will display html mesg."); 
       console.error(err);
-    }
-  }; // end handleCreateBook 
+      setShowAlert(true);
+    } // end try-catch
+    // code same as Act26 to here
+
+    setUserFormData({
+      username: '',
+      email: '',
+      password: '',
+    });
+  }; // end handleFormSubmit
+  // code same as Act 26 except for 1 dhange up to here.
 
   return (
     <>
-      <div className="text-light bg-dark p-5">
-        <Container>
-    []      <h1>Enter New Incident In The Form </h1>
-          <Form onSubmit={handleFormSubmit}>
-            <Row>
-              <Col xs={12} md={8}>
-                <Form.Control
-                  name='searchInput'
-                  value={searchInput}
-                  onChange={(e) => setSearchInput(e.target.value)}
-                  type='text'
-                  size='lg'
-                  placeholder='Search for a book'
-                />
-              </Col>
-              <Col xs={12} md={4}>
-                <Button type='submit' variant='success' size='lg'>
-                  Submit Search
-                </Button>
-              </Col>
-            </Row>
-          </Form>
-        </Container>
-      </div>
+      {/* This is needed for the validation functionality above */}
+      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+        {/* show alert if server response is bad */}
+        <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
+          Something went wrong with your signup!
+        </Alert>
 
-      <Container>
-        <h2 className='pt-5'>
-          {searchedBooks.length
-            ? `Viewing ${searchedBooks.length} results:`
-            : 'Search for a book to begin'}
-        </h2>
-        <Row>
-          {searchedBooks.map((book) => {
-            return (
-              <Col md="4" key={book.bookId}>
-                <Card border='dark'>
-                  {book.image ? (
-                    <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' />
-                  ) : null}
-                  <Card.Body>
-                    <Card.Title>{book.title}</Card.Title>
-                    <p className='small'>Authors: {book.authors}</p>
-                    <Card.Text>{book.description}</Card.Text>
-                    {Auth.loggedIn() && (
-                      <Button
-                        disabled={savedBookIds?.some((savedBookId) => savedBookId === book.bookId)}
-                        className='btn-block btn-info'
-                        onClick={() => handleSaveBook(book.bookId)}>
-                        {savedBookIds?.some((savedBookId) => savedBookId === book.bookId)
-                          ? 'This book has already been saved!'
-                          : 'Save this Book! (Old) '}
-                      </Button>                      
-                    )}   
-                    {Auth.loggedIn() && (
-                      <Button
-                        disabled={savedBookIds?.some((savedBookId) => savedBookId === book.bookId)}
-                        className='btn-block btn-info'
-                        onClick={() => handleCreateBook(book.bookId)}>
-                        {savedBookIds?.some((savedBookId) => savedBookId === book.bookId)
-                          ? 'This book has already been saved!'
-                          : 'Create Book!'}
-                      </Button>                      
-                    )}                    
-                  </Card.Body>
-                </Card>
-              </Col>
-            );
-          })}
-        </Row>
-      </Container>
+        <Form.Group className='mb-3'>
+          <Form.Label htmlFor='username'>Username</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='MJS Your username'
+            name='username'
+            onChange={handleInputChange}
+            value={userFormData.username}
+            required
+          />
+          <Form.Control.Feedback type='invalid'>Username is required!</Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group className='mb-3'>
+          <Form.Label htmlFor='email'>Email</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Your email address'
+            name='email'
+            onChange={handleInputChange}
+            value={userFormData.email}
+            required
+          />
+          <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group className='mb-3'>
+          <Form.Label htmlFor='password'>Password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Your password'
+            name='password'
+            onChange={handleInputChange}
+            value={userFormData.password}
+            required
+          />
+          <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
+        </Form.Group>
+        <Button
+          disabled={!(userFormData.username && userFormData.email && userFormData.password)}
+          type='submit'
+          variant='success'>
+          Submit
+        </Button>
+      </Form>
     </>
-  );
-};
+  ); // end return html-react code
+}; // end signupForm
 
 export default NewFightForm;
---
+++ client/src/pages/NewFightFormOld.jsx
@@ -0,0 +1,185 @@
+// MJS 5.2.24 - client/src/pages/NewFightFrom.jsx from MJS hw21 and uri starter code. 
+// Goal: Get create working.  Then create a form to accept data. 
+import { useState, useEffect } from 'react';
+import { Container, Col, Form, Button, Card, Row } from 'react-bootstrap';
+import Auth from '../utils/auth';
+
+// import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';  // likely need to search Google
+import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+// next 3 lines from commentForm26 
+import { Link } from 'react-router-dom';
+import { useQuery, useMutation } from '@apollo/client';
+// import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
+import { CREATE_BOOK } from '../utils/mutations';
+import { GET_ME } from '../utils/queries';
+
+const NewFightForm = ( ) => { 
+  // set initial form state
+  const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+  // set state for alert
+  const [showAlert, setShowAlert] = useState(false);
+  // create state for holding our search field data
+  const [searchInput, setSearchInput] = useState('');
+  // create state to hold saved bookId values
+  // const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+  // const [createBook, { error }] = useMutation(CREATE_BOOK); // must be outside handleForm promise
+  // const [addThought, { error }] = useMutation(ADD_THOUGHT, 
+  //   {refetchQueries: [QUERY_THOUGHTS, 'getThoughts', QUERY_ME, 'me']});
+  const [createBook, { error }] = useMutation(CREATE_BOOK, 
+        {refetchQueries: [GET_ME, 'me']});
+
+  const handleInputChange = (event) => {
+      const { name, value } = event.target;
+      setUserFormData({ ...userFormData, [name]: value });
+  }; 
+
+  // Sample from SingleThough26 
+  // const { loading, data } = useQuery(QUERY_SINGLE_THOUGHT, { variables: { thoughtId: thoughtId }, });
+  // Added next line for GraphQL. Basically find out who is logged in. // must be outside handleForm promise
+  // God only knows why this works with "data" but not "dataMe". 
+  const { loading, data } = useQuery(GET_ME, { variables: {  }, });  
+  console.log("NewFight GET_ME returned data ", data); 
+
+  let good = false; 
+  if (!!data && data.me && data.me.username) {
+        console.log("SearchBooks GET_ME result contains data.me.username"); 
+        good = true;
+      }
+  let username = "MJS"; 
+  if (good) {
+    username = data.me.username;
+  } else {
+    console.log("NewFightForm query GET_USER returned no data: ", data); 
+    console.log("Setting username to MJS"); 
+  } 
+  console.log("NewFightForm GET_ME returned username ", username);  // works with data
+
+  // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
+  // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
+  // useEffect(() => {
+  //   return () => saveBookIds(savedBookIds);
+  // });  // end useEffect
+
+  // create method to create new record (and insert ptr to it in User.myFights)
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log("NewFightForm.jsx handleFormSubmit saving for userID ", username); 
+    // get token
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+    if (!token) {  return false;    }
+    try {
+        console.log("NewFight.jsx handleCreateBook got token ... creating book using token ", token); 
+        // Create the book to save by copying form data to vars 
+        const title = "Book One"; 
+        const bookId = "7449292";
+        const description = "Book One Desc"; 
+        const authors = [ "Author1" ]; 
+        const image = null; 
+        const link = null;
+        const comments = []; 
+        const bookToSave = { title, bookId, authors, description, image, link, comments }; 
+        console.log("NewFightForm.jsx handleFormSubmit title ", bookToSave.title, " gBookId ", bookToSave.bookId); 
+        const vars = { username: username, ...bookToSave}; 
+        console.log("NewFight.jsx handleCreateBook creating book using ", vars); 
+        //  const { data } = await login({variables: { ...formState },}); // Analagous line from LoginForm.jsx 
+        //  thoughtAuthor: Auth.getProfile().data.username,
+        const { data } = await createBook({variables: vars,});
+        console.log("Created book. Returned data: ", data); 
+        // if book successfully saves to user's account, save book id (only) to state
+        setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+        // setThoughtText('');
+      } catch (err) {
+        console.error(err);
+      }
+  };  // end handleFormSubmit 
+
+  // create function to handle creating a book to our database. Also create ptr in User
+  const handleCreateBook = async (bookId) => {
+    // find the book in `searchedBooks` state by the matching id
+    const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
+    console.log("NewFight.jsx handleCreateBook saving for userID ", username); 
+    console.log("NewFight.jsx handleCreateBook title ", bookToSave.title, " gBookId ", bookToSave.bookId); 
+    // get token
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+    if (!token) {
+      return false;
+    }
+    try {
+      console.log("NewFight.jsx handleCreateBook got token ... creating book using token ", token); 
+      // GraphQL method from CommentForm26 
+      // const { data } = await addComment({
+      //  variables: { thoughtId, commentText, commentAuthor: Auth.getProfile().data.username, }, });
+      // setCommentText('');
+      const vars = { username: username, ...bookToSave}; 
+      console.log("NewFight.jsx handleCreateBook creating book using ", vars); 
+      //  const { data } = await login({variables: { ...formState },}); // Analagous line from LoginForm.jsx 
+      const { data } = await createBook({variables: vars,});
+      console.log("Created book. Returned data: ", data); 
+      // if book successfully saves to user's account, save book id (only) to state
+      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+    } catch (err) {
+      console.error(err);
+    }
+  }; // end handleCreateBook 
+
+  return (
+    <>
+      {/* This is needed for the validation functionality above */}
+      <Form noValidate onSubmit={handleFormSubmit}>
+        {/* show alert if server response is bad */}
+        <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
+          Something went wrong with your input!
+        </Alert>
+
+        <Form.Group className='mb-3'>
+          <Form.Label htmlFor='username'>Username</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='MJS Your username'
+            name='username'
+            onChange={handleInputChange}
+            value={userFormData.username}
+            required
+          />
+          <Form.Control.Feedback type='invalid'>Username is required!</Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group className='mb-3'>
+          <Form.Label htmlFor='email'>Email</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Your email address'
+            name='email'
+            onChange={handleInputChange}
+            value={userFormData.email}
+            required
+          />
+          <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group className='mb-3'>
+          <Form.Label htmlFor='password'>Password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Your password'
+            name='password'
+            onChange={handleInputChange}
+            value={userFormData.password}
+            required
+          />
+          <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
+        </Form.Group>
+        <Button
+          disabled={!(userFormData.username && userFormData.email && userFormData.password)}
+          type='submit'
+          variant='success'>
+          Submit
+        </Button>
+      </Form>
+    </>
+  ); // end return html-react code
+};
+
+export default NewFightForm;
