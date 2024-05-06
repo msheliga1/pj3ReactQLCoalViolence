6aafc415e9a00230622cb56ab0171081fff18eea
Mike Sheliga <msheliga1@gmail.com>
2024-05-05 22:38:21 -0400

    Got new rec created with Username hardwired. Great

--- client/src/pages/NewFightForm.jsx
+++ client/src/pages/NewFightForm.jsx
@@ -3,8 +3,9 @@ import { Form, Button, Alert } from 'react-bootstrap';
 
 // First change compared to Act26 (at one time was looking at WRONG file)
 // import { createUser } from '../utils/API';
-import { useMutation } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { ADD_USER2 } from '../utils/mutations';  // correct name 4.23.24
+import { GET_ME } from '../utils/queries';
 import { CREATE_BOOK } from '../utils/mutations';
 import Auth from '../utils/auth';
 
@@ -16,49 +17,51 @@ const NewFightForm = () => {
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
   // "Convert" ADD_USER2 mutation to addUser method
-  const [addUser, { error, data }] = useMutation(ADD_USER2);
+  // const [addUser, { error, data }] = useMutation(ADD_USER2);
+  // const [createBook, { error }] = useMutation(CREATE_BOOK); // must be outside handleForm promise
+  // const [addThought, { error }] = useMutation(ADD_THOUGHT, 
+  //   {refetchQueries: [QUERY_THOUGHTS, 'getThoughts', QUERY_ME, 'me']});
+  const [createBook, { error }] = useMutation(CREATE_BOOK, 
+    {refetchQueries: [GET_ME, 'me']});
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
   }; 
+  // const username = "s8"; 
 
+  // Method to create new record (and insert ptr to it in User.myFights)
   const handleFormSubmit = async (event) => {
-    console.log("handleFormSubmit (SignUpForm.jsx) starting ... "); 
     event.preventDefault();
-    // check if form has everything (as per react-bootstrap docs)
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
+    const username = "s9"; 
+    console.log("NewFightForm.jsx handleFormSubmit saving for userID ", username); 
+    // get token
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+    if (!token) {  return false;    }
     try {
-      // These lines generally changed REST->GQL 
-      // const response = await createUser(userFormData);
-      console.log("handleFormSubmit trying to ADD_USER2 with userFromData ", userFormData);
-      // Next line a poor convert of old method.  Can't treeat ADD_USER2(pararm) as a method. 
-      // const response = await ADD_USER2(userFormData); // No Good 
-      const { data } = await addUser({variables: { ...userFormData },});
-      console.log("handleFormSubmit returned from addUser done: ", data); 
-
-      Auth.login(data.addUser.token);
-      // Auth.login(token);
-      console.log("handleFormSubmit returned from login done: ");
-    } catch (err) {
-      console.log("handleFromSubmit catch an err ... setting ShowAlert to true, will display html mesg."); 
-      console.error(err);
-      setShowAlert(true);
-    } // end try-catch
-    // code same as Act26 to here
-
-    setUserFormData({
-      username: '',
-      email: '',
-      password: '',
-    });
-  }; // end handleFormSubmit
-  // code same as Act 26 except for 1 dhange up to here.
-
+        console.log("NewFight.jsx handleCreateBook got token ... creating book using token ", token); 
+        // Create the book to save by copying form data to vars 
+        const title = "Book1 NewFightForm"; 
+        const bookId = "7449292";
+        const description = "Book One Desc"; 
+        const authors = [ "Author1" ]; 
+        const image = null; 
+        const link = null;
+        const comments = []; 
+        const bookToSave = { title, bookId, authors, description, image, link, comments }; 
+        console.log("NewFightForm.jsx handleFormSubmit title ", bookToSave.title, " gBookId ", bookToSave.bookId); 
+        const vars = { username: username, ...bookToSave}; 
+        console.log("NewFight.jsx handleCreateBook creating book using ", vars); 
+        //  const { data } = await login({variables: { ...formState },}); // Analagous line from LoginForm.jsx 
+        //  thoughtAuthor: Auth.getProfile().data.username,
+        const { data } = await createBook({variables: vars,});
+        console.log("Created book. Returned data: ", data); 
+        // setThoughtText('');
+      } catch (err) {
+        console.error(err);
+      } 
+    };  // end handleFormSubmit 
+  
   return (
     <>
       {/* This is needed for the validation functionality above */}
